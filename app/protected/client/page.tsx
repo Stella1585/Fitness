fa6d357185a56272cefe1a9841dac46dd63f7f86
R. Bendinger <65312605+ByteWizard00@users.noreply.github.com>
2023-12-01 14:10:25 +0100

    deletion of unused code

--- app/protected/client/page.tsx
+++ app/protected/client/page.tsx
@@ -3,13 +3,5 @@ import ClientTabs from "@/components/client/client-tabs";
 import { redirect } from "next/navigation";
 
 export default async function ClientPage() {
-  // const session = await auth();
-
-  //@ts-ignore
-  // if (session?.user?.role != "Client") redirect("/");
-  //@ts-ignore
-  // if (session?.user?.role != "Client")
-  //   return <p>You are not authorized to view this page!</p>;
-
   return <ClientTabs />;
 }
--- app/protected/manager/page.tsx
+++ app/protected/manager/page.tsx
@@ -3,12 +3,6 @@ import { Session } from "next-auth";
 import ManagerTabs from "@/components/manager/manager-tabs";
 
 export default async function ManagerPage() {
-  const session: Session | null = await auth();
-
-  //@ts-ignore
-  // if ((!session || !session?.user) && session?.user?.role !== "Manager")
-  //   return <p>You are not authorized to view this page!</p>;
-
   return (
     <div>
       <p className="mb-10">You are an Manager, welcome!</p>
